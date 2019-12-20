c70b9bf7612a20cb3c1d954e04ee0411a75c5a83
Marvio Rocha <marviorocha@marviorocha.com>
2019-12-19 21:27:25 -0300

    work today finished

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $(".navbar-burger").click(function() {
   
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-menu").addClass("animated fadeIn");
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
   
